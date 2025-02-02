ab70094b79e025129aacc6069c238a9a6b77ae5c
utkarsh2023021 <168927514+utkarsh2023021@users.noreply.github.com>
2025-02-03 01:27:54 +0530

    Update signup.js

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -20,7 +20,7 @@ const Signup = () => {
 
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:5000/signup", {
+      const response = await fetch("https://twondassmm.onrender.com/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
