9705815b822425572b60da7c5ba0d994454f0ad5
utkarsh2023021 <168927514+utkarsh2023021@users.noreply.github.com>
2025-02-03 14:56:03 +0530

    Update room.js

--- server/model/room.js
+++ server/model/room.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const roomSchema = new mongoose.Schema({
   roomId: { type: String, required: true, unique: true },
   roomName: { type: String, required: true },
-  users: { type: Number, default: 0 }, // Track the number of users in the room
+  users: { type: Number, default: 0 },
 });
 
 const Room = mongoose.model("Room", roomSchema);
